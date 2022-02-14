b988dcbf8ba29644508dfe15ffce940d685e704a
Rhea Paul <73695038+RheaPaul@users.noreply.github.com>
2022-02-14 16:37:09 +0530

    Create Program9.c
    Added Program9

---
+++ Programs/Shell-Scripting/Program9.c
@@ -0,0 +1,29 @@
+#!/bin/Sh
+#Write a shell script to calculate the time difference in seconds 
+#when two time values are provided in hh:mm:ss format.
+echo "Enter the first time "
+echo "Hour: "
+read h1
+echo "Minutes: "
+read m1
+echo "Seconds: "
+read s1
+echo "Enter the second time "
+echo "Hour: "
+read h2
+echo "Minutes: "
+read m2
+echo "Seconds: "
+read s2
+Hour1=`expr $h1 \* 3600`
+Hour2=`expr $h2 \* 3600`
+Min1=`expr $m1 \* 60`
+Min2=`expr $m2 \* 60`
+Second1=$(expr $Hour1 + $Min1 + $s1)
+Second2=$(expr $Hour2 + $Min2 + $s2)
+if [ $Second1 -ge $Second2 ]; then
+    TimeDiff=$(expr $Second1 - $Second2)
+else
+    TimeDiff=$(expr $Second2 - $Second1)
+fi 
+echo "The time difference is $TimeDiff seconds"
